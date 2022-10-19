e8baa74a7f463013b1f8f47e2cfeab4102fbdb16
Eva Reitzel <evareitzel@gmail.com>
2022-10-18 20:47:54 -0400

    Done.

---
+++ public/note.js
@@ -0,0 +1,29 @@
+// App
+  // SushiContainer
+    // Sushi
+    // MoreButton
+  //  Table 
+
+
+
+
+// from SushiContainer.js
+<div className="belt">
+{/* Render Sushi components here! */}
+
+// from Sushi.js
+<div className="plate" onClick={/* Give me a callback! */ null}>
+{/* // fetch delete // filter // setSushis(filtered) */}
+
+  src=({/* Give me an image source! */ null})
+  alt={/* Give me a name! */ "Sushi"}
+          {/* Give me a name! */} - ${/* Give me a price! */}
+  <div className="plate" onClick={/* Give me a callback! */ null}></div>
+
+// from More.js
+function MoreButton(onClickMore) {
+  return <button onClick={/* Fill me in! */ null}>More sushi!</button>;
+}
+
+// wallet
+You have: ${/* Give me how much money I have left */} remaining!
